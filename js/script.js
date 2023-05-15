2c053ab5437b1d969f137df5520c8eff19c5eada
emanuelefarelli <emaaliens888@gmail.com>
2023-05-15 15:54:13 +0200

    Raggiunta la prima milestone

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const myButton = document.querySelector('div.button');
+let ticketPrice;
+let priceYetCalculated = false;
+
+myButton.addEventListener('click',
+    function(){
+        if(priceYetCalculated == false){
+
+            const totKm = document.getElementById('tot-km');
+            const userAge = document.getElementById('user-age');  
+            ticketPrice = totKm.value * 0.233;
+
+            if(parseInt(userAge.value) > 65){
+
+                const priceDiscount = (37.7 / 100) * ticketPrice;
+                ticketPrice -= priceDiscount;
+
+            }else if(parseInt(userAge.value) < 18){
+
+                const priceDiscount = (19.4 / 100) * ticketPrice;
+                ticketPrice -= priceDiscount;
+
+            }else if((userAge.value == undefined)||(userAge.value < 0)){
+
+                alert("L'età inserita non è valida!");
+                return;
+            }
+
+            console.log(userAge.value)
+            console.log("Il prezzo del tuo biglietto è: ",ticketPrice)
+            priceYetCalculated = true;
+        }
+    }
+);
